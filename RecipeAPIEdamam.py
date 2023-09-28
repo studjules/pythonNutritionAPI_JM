3416a86074331074979acdc80ac688b9dba11bc3
JulianMagnago <julian.magnago@student.uibk.ac.at>
2023-09-28 13:49:00 +0200

    Recipe search functions for Breakfast Lunch and Dinner initialized

--- RecipeAPIEdamam.py
+++ RecipeAPIEdamam.py
@@ -6,42 +6,138 @@ class VeganRecipe:
         self.app_key = app_key
         self.base_url = 'https://api.edamam.com/api/recipes/v2'
 
-    def search_recipes(self, query):
-        try:
-            # Define your query parameters
-            query_params = {
-                'ingr': query,
-                'app_id': self.app_id,
-                'app_key': self.app_key
+    def search_vegan_breakfast_recipe(self):
+        # Set the endpoint and query parameters
+        endpoint = self.base_url
+        health_labels = []
+        diet = ["high-protein"]
+        meal_type = ["breakfast"]
+        dish_Type = ["Main course"]
+        params = {
+            "type": "public",
+            "health": health_labels,
+            "app_id": self.app_id,
+            "app_key": self.app_key,
+            "diet": diet,
+            "mealType": meal_type,
+            "dishType": dish_Type,
+        }
+
+        # Make the API request
+        response = requests.get(endpoint, params=params)
+
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            # Parse and work with the response JSON data
+            data = response.json()
+            # Access the first recipe in the response
+            br_recipes = [hit["recipe"] for hit in data.get("hits", [])[:100]]
+
+            # Extract and return information about the first recipe
+            return {
+                "Recipe Name": br_recipes[0].get("label"),
+                "Recipe Image": br_recipes[0].get("image"),
+                "Recipe Ingredients": br_recipes[0].get("ingredientLines"),
+                "URL": br_recipes[0].get("url"),
+                "Listlength": len(br_recipes),
             }
+        else:
+            # Handle API request error (e.g., return an error message)
+            return {"Error": response.status_code, "Message": response.text}
 
-            # Make a GET request to the Edamam API
-            response = requests.get(self.base_url, params=query_params)
+    def search_vegan_lunch_recipe(self):
+        # Set the endpoint and query parameters
+        endpoint = self.base_url
+        health_labels = []
+        diet = ["high-protein"]
+        meal_type = ["lunch"]
+        dish_Type = ["Main course"]
+        params = {
+            "type": "public",
+            "health": health_labels,
+            "app_id": self.app_id,
+            "app_key": self.app_key,
+            "diet": diet,
+            "mealType": meal_type,
+            "dishType": dish_Type,
+        }
 
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                data = response.json()
-                return data
-            else:
-                return {"error": response.status_code, "message": response.text}
+        # Make the API request
+        response = requests.get(endpoint, params=params)
 
-        except requests.exceptions.RequestException as e:
-            return {"error": "RequestException", "message": str(e)}
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            # Parse and work with the response JSON data
+            data = response.json()
+            # Access the first recipe in the response
+            lu_recipes = [hit["recipe"] for hit in data.get("hits", [])[:100]]
+
+            # Extract and return information about the first recipe
+            return {
+                "Recipe Name": lu_recipes[2].get("label"),
+                "Recipe Image": lu_recipes[2].get("image"),
+                "Recipe Ingredients": lu_recipes[2].get("ingredientLines"),
+                "URL": lu_recipes[2].get("url"),
+                "Listlength": len(lu_recipes),
+            }
+        else:
+            # Handle API request error (e.g., return an error message)
+            return {"Error": response.status_code, "Message": response.text}
+
+    def search_vegan_dinner_recipe(self):
+        # Set the endpoint and query parameters
+        endpoint = self.base_url
+        health_labels = []
+        diet = ["high-protein"]
+        meal_type = ["dinner"]
+        dish_Type = ["Main course"]
+        params = {
+            "type": "public",
+            "health": health_labels,
+            "app_id": self.app_id,
+            "app_key": self.app_key,
+            "diet": diet,
+            "mealType": meal_type,
+            "dishType": dish_Type,
+        }
+
+        # Make the API request
+        response = requests.get(endpoint, params=params)
+
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            # Parse and work with the response JSON data
+            data = response.json()
+            # Access the first recipe in the response
+            di_recipes = [hit["recipe"] for hit in data.get("hits", [])[:100]]
+
+            # Extract and return information about the first recipe
+            return {
+                "Nutrients": di_recipes[1].get("nutrients[CA]"),
+                "Recipe Name": di_recipes[1].get("label"),
+                "Recipe Image": di_recipes[1].get("image"),
+                "Recipe Ingredients": di_recipes[1].get("ingredientLines"),
+                "URL": di_recipes[1].get("url"),
+                "Listlength": len(di_recipes),
+
+            }
+        else:
+            # Handle API request error (e.g., return an error message)
+            return {"Error": response.status_code, "Message": response.text}
 
 
-if __name__ == '__main__':
-    app_id = '81f8d87f'
-    app_key = '25e930457335a2fdcf2d293c6de3fcde'
+if __name__ == "__main__":
+    # Example usage:
+    app_id = "81f8d87f"
+    app_key = "25e930457335a2fdcf2d293c6de3fcde"
+    recipe_searcher = VeganRecipe(app_id, app_key)
 
-    # Create an instance of the EdamamAPI class
-    veg_rec = VeganRecipe(app_id, app_key)
+    recipe_breakfast = recipe_searcher.search_vegan_breakfast_recipe()
+    recipe_lunch = recipe_searcher.search_vegan_lunch_recipe()
+    recipe_dinner = recipe_searcher.search_vegan_dinner_recipe()
 
-    # Perform a recipe search
-    query = 'potatog'
-    response_data = veg_rec.search_recipes(query)
+    print(recipe_breakfast)
+    print(recipe_lunch)
+    print(recipe_dinner)
 
-    # Check and print the API response
-    if 'error' in response_data:
-        print("Error:", response_data['error'], response_data['message'])
-    else:
-     print("API Response:", response_data)
+
